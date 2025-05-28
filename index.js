7dd56da01e480b79cb8cc059f3ab07da0b8e89be
Whitelotuscorp <contact@whitelotuscorporation.com>
2025-05-28 12:31:59 +0530

    feat: First commit

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const color = require('./src/helper/color.helper')
 const startEmailConsumer = require('./src/consumers/email.consumer')
 
 startEmailConsumer().then(() => {
-  color.success('Email-service running - listening to RabbitMQ...')
+  color.success('Email-service running - listening to RabbitMQ....')
 })
